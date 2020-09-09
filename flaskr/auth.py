f58180d7986bf7ef22895c851a7b02f6ddc88267
ganatan <dannyganatan@gmail.com>
2020-09-09 11:49:18 +0200

    Update SQL & comments

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -17,8 +17,6 @@ bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 
 def login_required(view):
-    """View decorator that redirects anonymous users to the login page."""
-
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
@@ -31,8 +29,6 @@ def login_required(view):
 
 @bp.before_app_request
 def load_logged_in_user():
-    """If a user id is stored in the session, load the user object from
-    the database into ``g.user``."""
     print('0007')
     user_id = session.get("user_id")
 
@@ -46,11 +42,6 @@ def load_logged_in_user():
 
 @bp.route("/register", methods=("GET", "POST"))
 def register():
-    """Register a new user.
-
-    Validates that the username is not already taken. Hashes the
-    password for security.
-    """
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
@@ -85,7 +76,6 @@ def register():
 
 @bp.route("/login", methods=("GET", "POST"))
 def login():
-    """Log in a registered user by adding the user id to the session."""
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
@@ -101,7 +91,6 @@ def login():
             error = "Incorrect password."
 
         if error is None:
-            # store the user id in a new session and return to the index
             session.clear()
             session["user_id"] = user["id"]
             return redirect(url_for("index"))
@@ -113,6 +102,5 @@ def login():
 
 @bp.route("/logout")
 def logout():
-    """Clear the current session, including the stored user id."""
     session.clear()
     return redirect(url_for("index"))
