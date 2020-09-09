2cb33d6f37c56e163cbffd40f2aaeaa8d1376726
ganatan <dannyganatan@gmail.com>
2020-09-09 11:35:00 +0200

    Update apache2 config

--- app.wsgi
+++ app.wsgi
@@ -3,4 +3,4 @@ import sys
 logging.basicConfig(stream=sys.stderr)
 sys.path.insert(0, '/home/services/flask-frontend/')
 from app import app as application
-application.secret_key = 'anything you wish'
+application.secret_key = 'gandalfisamagician'
--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -7,7 +7,7 @@ def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY="dev",
+        SECRET_KEY="gandalfisamagician",
         DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
     )
 
